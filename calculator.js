e93113e67ad72ed7ad32503826d511d7dfed9fe9
Muhammed Ibrahim Amanokhai <ibrahimjunior08@gmail.com>
2023-06-28 12:32:21 +0000

    iterating my calculator project

--- calculator.js
+++ calculator.js
@@ -2,7 +2,12 @@ function Clear() {
     var b = document.getElementById("answer");
     b.value = '';
 }
-
+function percent() {
+    calc.display.value = calc.display.value/100;
+}
+function cube() {
+    calc.display.value = calc.display.value ** 3;
+}
 function Return() {
     var R = document.getElementById("answer");
     R.value = R.value.slice(0,-1);
@@ -15,6 +20,7 @@ function Result() {
     }
     catch(error) {
         display.value = "Error";
+        alert("invalid expression please check")
     }
 }
 function sqrt() {
